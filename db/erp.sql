1c88fc2a6c0dc459e94ee3ab9b58ed4d489cd257
Nazmul Basher <nazmul.basher@fieldnation.com>
2014-10-21 12:29:23 +0600

    SQL

--- db/erp.sql
+++ db/erp.sql
@@ -31,7 +31,7 @@ CREATE TABLE `tbl_product` (
 
 /*Data for the table `tbl_product` */
 
-insert  into `tbl_product`(`pid`,`name`,`current_stock`,`rate`,`unit`) values (1,'Pen',10,12,'pcs'),(2,'Pencil',12,5,'pcs');
+insert  into `tbl_product`(`pid`,`name`,`current_stock`,`rate`,`unit`) values (1,'Pen',90,12,'pcs'),(2,'Pencil',48,5,'pcs');
 
 /*Table structure for table `tbl_product_out` */
 
@@ -41,20 +41,59 @@ CREATE TABLE `tbl_product_out` (
   `pout_id` int(11) NOT NULL AUTO_INCREMENT,
   `pid` int(11) DEFAULT NULL,
   `rec_id` int(11) DEFAULT NULL,
+  `req_det_id` int(11) DEFAULT NULL,
   `qty` double DEFAULT NULL,
   `rate` double DEFAULT NULL,
   `out_date` datetime DEFAULT NULL,
   PRIMARY KEY (`pout_id`),
   KEY `fk_tbl_product_out_tbl_product1_idx` (`pid`),
   KEY `fk_tbl_product_out_tbl_product_rec1_idx` (`rec_id`),
+  KEY `fk_tbl_product_out_tbl_product_req_details1_idx` (`req_det_id`),
   CONSTRAINT `fk_tbl_product_out_tbl_product1` FOREIGN KEY (`pid`) REFERENCES `tbl_product` (`pid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_tbl_product_out_tbl_product_rec1` FOREIGN KEY (`rec_id`) REFERENCES `tbl_product_rec` (`rec_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_tbl_product_out_tbl_product_rec2` FOREIGN KEY (`rec_id`) REFERENCES `tbl_product_rec` (`rec_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_tbl_product_out_tbl_product_rec2` FOREIGN KEY (`rec_id`) REFERENCES `tbl_product_rec` (`rec_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_tbl_product_out_tbl_product_req_details1` FOREIGN KEY (`req_det_id`) REFERENCES `tbl_product_req_details` (`req_det_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 /*Data for the table `tbl_product_out` */
 
-insert  into `tbl_product_out`(`pout_id`,`pid`,`rec_id`,`qty`,`rate`,`out_date`) values (1,1,1,2,10.5,'2014-10-21 17:57:52'),(2,2,2,4,5,'2014-10-21 17:58:11');
+insert  into `tbl_product_out`(`pout_id`,`pid`,`rec_id`,`req_det_id`,`qty`,`rate`,`out_date`) values (1,1,1,1,10,12,'2014-10-21 12:25:16'),(2,2,2,2,2,5,'2014-10-21 12:25:40');
+
+/*Table structure for table `tbl_product_purchase_req` */
+
+DROP TABLE IF EXISTS `tbl_product_purchase_req`;
+
+CREATE TABLE `tbl_product_purchase_req` (
+  `pur_req_id` int(11) NOT NULL AUTO_INCREMENT,
+  `pur_req_date` date DEFAULT NULL,
+  `pur_req_by` int(11) DEFAULT NULL,
+  `status` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`pur_req_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `tbl_product_purchase_req` */
+
+insert  into `tbl_product_purchase_req`(`pur_req_id`,`pur_req_date`,`pur_req_by`,`status`) values (1,'2014-10-21',1,1);
+
+/*Table structure for table `tbl_product_purchase_req_details` */
+
+DROP TABLE IF EXISTS `tbl_product_purchase_req_details`;
+
+CREATE TABLE `tbl_product_purchase_req_details` (
+  `pur_req_det_id` int(11) NOT NULL AUTO_INCREMENT,
+  `pur_req_id` int(11) DEFAULT NULL,
+  `pid` int(11) DEFAULT NULL,
+  `qty` double DEFAULT NULL,
+  PRIMARY KEY (`pur_req_det_id`),
+  KEY `fk_tbl_product_req_details_tbl_product1_idx` (`pid`),
+  KEY `fk_tbl_product_req_details_tbl_product_req1_idx` (`pur_req_id`),
+  CONSTRAINT `fk_tbl_product_req_details_tbl_product1` FOREIGN KEY (`pid`) REFERENCES `tbl_product` (`pid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_tbl_product_req_details_tbl_product_req1` FOREIGN KEY (`pur_req_id`) REFERENCES `tbl_product_purchase_req` (`pur_req_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `tbl_product_purchase_req_details` */
+
+insert  into `tbl_product_purchase_req_details`(`pur_req_det_id`,`pur_req_id`,`pid`,`qty`) values (1,1,1,100),(2,1,2,50);
 
 /*Table structure for table `tbl_product_rec` */
 
@@ -62,19 +101,19 @@ DROP TABLE IF EXISTS `tbl_product_rec`;
 
 CREATE TABLE `tbl_product_rec` (
   `rec_id` int(11) NOT NULL AUTO_INCREMENT,
-  `req_det_id` int(11) DEFAULT NULL,
+  `pur_req_det_id` int(11) DEFAULT NULL,
   `qty` double DEFAULT NULL,
   `rate` double DEFAULT NULL,
   `rec_date` datetime DEFAULT NULL,
   `qty_disburse` double DEFAULT NULL,
   PRIMARY KEY (`rec_id`),
-  KEY `fk_tbl_product_rec_tbl_product_req_details1_idx` (`req_det_id`),
-  CONSTRAINT `fk_tbl_product_rec_tbl_product_req_details1` FOREIGN KEY (`req_det_id`) REFERENCES `tbl_product_req_details` (`req_det_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `fk_tbl_product_rec_tbl_product_purchase_req_details1_idx` (`pur_req_det_id`),
+  CONSTRAINT `fk_tbl_product_rec_tbl_product_purchase_req_details1` FOREIGN KEY (`pur_req_det_id`) REFERENCES `tbl_product_purchase_req_details` (`pur_req_det_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 /*Data for the table `tbl_product_rec` */
 
-insert  into `tbl_product_rec`(`rec_id`,`req_det_id`,`qty`,`rate`,`rec_date`,`qty_disburse`) values (1,1,2,11,'2014-10-20 17:51:11',2),(2,2,4,5,'2014-10-20 17:51:57',4);
+insert  into `tbl_product_rec`(`rec_id`,`pur_req_det_id`,`qty`,`rate`,`rec_date`,`qty_disburse`) values (1,1,100,12,'2014-10-21 12:23:20',10),(2,2,50,5,'2014-10-21 12:23:48',2);
 
 /*Table structure for table `tbl_product_req` */
 
@@ -91,7 +130,7 @@ CREATE TABLE `tbl_product_req` (
 
 /*Data for the table `tbl_product_req` */
 
-insert  into `tbl_product_req`(`req_id`,`req_date`,`req_by`,`status`,`req_required_date`) values (1,'2014-10-20',1,1,'2014-10-21');
+insert  into `tbl_product_req`(`req_id`,`req_date`,`req_by`,`status`,`req_required_date`) values (1,'2014-10-21',1,1,'2014-10-22');
 
 /*Table structure for table `tbl_product_req_details` */
 
@@ -103,15 +142,15 @@ CREATE TABLE `tbl_product_req_details` (
   `pid` int(11) DEFAULT NULL,
   `qty` double DEFAULT NULL,
   PRIMARY KEY (`req_det_id`),
-  KEY `fk_tbl_product_req_details_tbl_product1_idx` (`pid`),
-  KEY `fk_tbl_product_req_details_tbl_product_req1_idx` (`req_id`),
-  CONSTRAINT `fk_tbl_product_req_details_tbl_product1` FOREIGN KEY (`pid`) REFERENCES `tbl_product` (`pid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_tbl_product_req_details_tbl_product_req1` FOREIGN KEY (`req_id`) REFERENCES `tbl_product_req` (`req_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `fk_tbl_product_req_details_tbl_product_req2_idx` (`req_id`),
+  KEY `fk_tbl_product_req_details_tbl_product2_idx` (`pid`),
+  CONSTRAINT `fk_tbl_product_req_details_tbl_product_req2` FOREIGN KEY (`req_id`) REFERENCES `tbl_product_req` (`req_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_tbl_product_req_details_tbl_product2` FOREIGN KEY (`pid`) REFERENCES `tbl_product` (`pid`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 /*Data for the table `tbl_product_req_details` */
 
-insert  into `tbl_product_req_details`(`req_det_id`,`req_id`,`pid`,`qty`) values (1,1,1,2),(2,1,2,4);
+insert  into `tbl_product_req_details`(`req_det_id`,`req_id`,`pid`,`qty`) values (1,1,1,10),(2,1,2,2);
 
 /*Table structure for table `tbl_user` */
 
@@ -128,11 +167,11 @@ CREATE TABLE `tbl_user` (
   `image` varchar(255) DEFAULT NULL,
   `status` tinyint(4) DEFAULT '1',
   PRIMARY KEY (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 /*Data for the table `tbl_user` */
 
-insert  into `tbl_user`(`uid`,`uname`,`first_name`,`last_name`,`password`,`email`,`phone`,`image`,`status`) values (1,'admin','Nazmul','Basher','admin','nazmul.basher@fieldnation.com','+8801712994064',NULL,1);
+insert  into `tbl_user`(`uid`,`uname`,`first_name`,`last_name`,`password`,`email`,`phone`,`image`,`status`) values (1,'admin','Nazmul','Basher','admin','nazmul.basher@fieldnation.com','+8801712994064',NULL,1),(2,'alamin','Al','Amin','alamin','enalamin@gmail.com','+8801677073455',NULL,1);
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
